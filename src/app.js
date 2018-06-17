0c43b4379b4b747946d7fd88a2dd8de4574b4a82
Ilya Meerovich <ilya.meerov@gmail.com>
2018-06-16 20:41:56 -0700

    Added React Router and added basic routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 
 
 import './styles/styles.scss'
 
+
+
+const HomeDashboard = () => <div><p>I am the home route!</p></div> ;
+
+const CreateExpensePage = () => <div><p>I am the Create route!</p></div>;
+
+const EditExpensePage = () => <div><p>I am the  Edit route!</p></div>;
+
+const HelpPage = () => <div><p>I am the Help Page route!</p></div>;
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={HomeDashboard} exact={true}/>
+      <Route path="/create" component={CreateExpensePage}/>
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+    </div>
+  </BrowserRouter>
+)
+
 const root = document.getElementById('root');
-render(<p>Test Test</p>, root);  
+render(routes, root);  
