9f4843a71f47514e1e7990b70afbcb5fe1a24082
Ilya Meerovich <ilya.meerov@gmail.com>
2018-06-16 17:51:48 -0700

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+
+import './styles/styles.scss'
+
+const root = document.getElementById('root');
+render(<p>Test Test</p>, root);  
